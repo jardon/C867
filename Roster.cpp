70b4cb46352a2443f1227c984b646ca5a363c788
Jarred Wilson <jarred.wilson12@gmail.com>
2020-02-06 17:05:52 -0500

    Fix algorithm for splitting data string

--- Roster.cpp
+++ Roster.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 int main() {
 
-    const string studentData[] = {"A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY",
+    const string studentData[] = {"A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY,",
                                   "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
                                   "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
                                   "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
@@ -23,28 +23,30 @@ int main() {
         size_t last = 0;
         size_t next = 0;
         string sub;
-        string tempData[9] = {"string", "string", "string", "string", "string", "string", "string", "string", "string"};
+        string tempData[9];
         int j = 0;
-        int degree;
+        Degree degree;
 
-//        while((next = studentData[i].find(delimiter, last)) != string::npos) {
-//            sub = studentData[i].substr(last, next - last);
-//            tempData[j++] = sub;
-//            last = next++;
-//        }
-//
-//        if(tempData[8] == "SOFTWARE")
-//            degree = SOFTWARE;
-//        else if (tempData[8] == "NETWORK")
-//            degree = NETWORK;
-//        else if (tempData[8] == "SECURITY")
-//            degree = SECURITY;
+        while((next = studentData[i].find(delimiter, last)) != string::npos) {
+            sub = studentData[i].substr(last, next - last);
+            tempData[j] = sub;
+            j++;
+            last = next + 1;
+        }
+        tempData[8] = studentData[i].substr(last, studentData[i].length() - 1);
 
-//        roster.add(tempData[0], tempData[1], tempData[2], tempData[3], stoi(tempData[4]), \
-//               stoi(tempData[5]), stoi(tempData[6]), stoi(tempData[7]), degree);
+        if(tempData[8] == "SOFTWARE")
+            degree = SOFTWARE;
+        else if (tempData[8] == "NETWORK")
+            degree = NETWORK;
+        else if (tempData[8] == "SECURITY")
+            degree = SECURITY;
+
+        roster.add(tempData[0], tempData[1], tempData[2], tempData[3], stoi(tempData[4]), \
+               stoi(tempData[5]), stoi(tempData[6]), stoi(tempData[7]), degree);
     }
 
-//    roster.printAll();
+    roster.printAll();
 //    roster.printInvalidEmails();
 //    roster.printDaysInCourse("A3");
 //    roster.printByDegreeProgram(SOFTWARE);
